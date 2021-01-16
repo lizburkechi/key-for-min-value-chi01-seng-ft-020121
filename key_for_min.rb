ba1d3da7ca6441595b6280cc3d7cb1eeb931b849
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 20:48:43 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,20 @@
 
 def key_for_min_value(name_hash)
 
+smallest_value = nil
+answer_hash = nil
+
+  name_hash.each do |key, value|
+
+    if smallest_value == nil
+      smallest_value = value
+      answer_hash = key
+    else
+      if smallest_value > value
+        smallest_value = value
+        answer_hash = key
+  end
+end
+end
+answer_hash
 end
