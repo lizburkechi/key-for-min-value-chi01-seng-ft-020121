3508be045085685bcc1d4c7c74c8c0059814203c
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 20:34:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-value = 0
+smallest_value = 0
+answer_hash = nil
 
   name_hash.each do |key, value|
-    if value < name_hash
-      puts
+
+    if smallest_value == 0
+      value < smallest_value
+      smallest_value = value
+      answer_hash = key
 
   end
-
 end
+answer_hash
 end
