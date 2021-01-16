e26aae096304e15a922d54d6fb15592ca95399ba
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 20:47:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+
 smallest_value = nil
 answer_hash = nil
 
@@ -11,7 +11,7 @@ answer_hash = nil
     if smallest_value == nil
       smallest_value = value
       answer_hash = key
-    else 
+    else
       if smallest_value > value
         smallest_value = value
         answer_hash = key
